792d71210db328de1c6d51c5c509bc7ad1f3cea5
Gunnm <1121859777@qq.com>
2025-02-11 20:51:41 +0800

    导入cxxopts

--- 命令行工具.cpp
+++ 命令行工具.cpp
@@ -1,32 +1,8 @@
 #include"basicCommandforFile.h"
+#include"cxxopts.hpp"
 
-int main() {
-    std::string dir_path = "D:\Test"; // 测试目录
-    std::string prefix = "doc_";
-    std::string suffix = "";
-    std::string new_extension = "bak";
-    std::string regex_pattern = ""; // 匹配数字
-    std::string replacement = "";
 
-    // 获取文件列表
-    auto files = get_files(dir_path);
-
-    // 重命名每个文件
-    for (const auto& file : files) {
-        std::string original_name = file.filename().string();
-        std::string new_name = generate_new_name(
-            original_name,
-            prefix,
-            suffix,
-            new_extension,
-            regex_pattern,
-            replacement
-        );
-
-        if (rename_file(file, new_name)) {
-            std::cout << "Renamed: " << original_name << " → " << new_name << "\n";
-        }
-    }
-    return 0;
+int main(int argc, char* argv[]) {
+	
 
 }
